5a92c6dc4a7c0e37788b2c8a30d190f7c783b74d
csienme <r13922142@csie.ntu.edu.tw>
2026-05-17 00:23:01 +0800

    delete

--- temp.bash
+++
@@ -1,2 +0,0 @@
-bash run_360.sh kitchen ../data/Other-360/kitchen/images ../data/Other-360/kitchen/object_masks
-bash run_360.sh pinecone ../data/Other-360/pinecone/images ../data/Other-360/pinecone/object_masks
